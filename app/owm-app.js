2cd43f4fac2e02bff2ecff99441948cef850a2b8
Apr7174 <apr.7174@gmail.com>
2017-01-08 14:12:11 -0500

    added files, cant read city

---
+++ app/owm-app.js
@@ -0,0 +1,19 @@
+angular.module('OWMApp', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl : 'home.html',
+            controller : 'HomeCtrl',
+            controllerAs: 'vm'
+        }).when('!/city', {
+            templateUrl : 'city.html',
+            controller : 'CityCtrl',
+            controllerAs: 'vm'
+        });
+    }])
+    .controller('HomeCtrl', [function() {
+        //empty for now
+    }])
+	.controller('CityCtrl', [function() {
+	    var vm = this;
+	    vm.city = 'New York';
+	}]);
