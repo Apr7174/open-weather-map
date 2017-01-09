651d4c667b028225228d712395ac9f817959e1c6
Apr7174 <apr.7174@gmail.com>
2017-01-09 17:35:10 -0500

    updated city links to inlcude ! to work in browser

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,19 +1,40 @@
 angular.module('OWMApp', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
+	.value('owmCities', ['New York', 'Dallas', 'Chicago'])
+
+
+    .config(['$routeProvider', function($routeProvider, $loactionProvider) {
         $routeProvider.when('/', {
             templateUrl : 'home.html',
             controller : 'HomeCtrl',
             controllerAs: 'vm'
-        }).when('/city', {
+        }).when('/cities/:city', {
             templateUrl : 'city.html',
             controller : 'CityCtrl',
-            controllerAs: 'vm'
+            controllerAs: 'vm',
+//            preloads the city to load before
+            resolve : {
+                city: function(owmCities, $route, $location) {
+                    var city = $route.current.params.city;
+                    if (owmCities.indexOf(city) === -1) {
+                        $location.path('/error');
+                        return;
+                    }
+                    return city;
+                }
+            }
+        }).when('/error', {
+            template: '<p>Error - City Not in List</p>'
+        }).otherwise('/error');
+    }])
+    .run(['$rootScope', '$location', function($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path('/error');
         });
     }])
     .controller('HomeCtrl', [function() {
         //empty for now
     }])
-	.controller('CityCtrl', [function() {
+	.controller('CityCtrl', ['$routeParams', function($routeParams) {
 	    var vm = this;
 	    vm.city = $routeParams.city;
 	}]);
